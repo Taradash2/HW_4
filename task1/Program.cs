b735dbb95eb8812608ec76f39f9ae3f003ec3e90
Danila <taradash98@gmail.com>
2022-12-15 19:11:19 +0300

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int n = a;
+
+for (int i = 1; i < b; i++)
+{
+n = n * a;
+}
+Console.WriteLine($"A в степени B равно: {n}");
